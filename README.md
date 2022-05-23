b89e7596c39262acec1c62927b85dcd89cdc33af
ttasjwi <ttasjwi920@gmail.com>
2022-05-23 22:21:31 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+## Study-Deploy
+- 목표 : `GitHub Action` + `CodeDeploy` + `Nginx`를 이용한 무중단 배포
+- 참고 링크 : https://wbluke.tistory.com/39
+
+---
+
---
+++ src/main/java/ttasjwi/studydeploy/StudyDeployApplication.java
@@ -0,0 +1,13 @@
+package ttasjwi.studydeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyDeployApplication.class, args);
+	}
+
+}
